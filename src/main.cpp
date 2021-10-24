8a57ccce60a5ee67cf11a7f4dcc16c59c4e75a6f
liyang <272891021@qq.com>
2021-10-24 16:03:34 +0800

    add udp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <Ticker.h>
+#include <WiFiUdp.h>
 #include <Wire.h>
+#define wifi WiFi
 Adafruit_SSD1306 display(128, 64, &Wire);
-Ticker ticker;
+Ticker ticker0;
+Ticker ticker1;
+WiFiUDP udp;
 
 void swap(int *p0, int *p1);
 void drawChess(int x0, int y0, bool *p);
@@ -13,6 +18,9 @@ void drawMap(long n);
 void pointer();
 void drawPointer(int x0, int y0);
 void swapchess();
+void connect();
+void send();
+void parse();
 
 bool chesspixels[7][25]{
     {1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1},
@@ -31,7 +39,14 @@ int chessmap[10][9]{
     {7, 0, 7, 0, 7, 0, 7, 0, 7},          {0, 6, 0, 0, 0, 0, 0, 6, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0},          {1, 2, 3, 4, 5, 4, 3, 2, 1}};
 
+IPAddress mainIP(192, 168, 1, 1);
+IPAddress getawayIP(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+const uint16_t port = 8888;
+
+bool master = 0;
 long n = 0;
+int i = 0;
 long time1 = 0;
 long time2 = 0;
 int selected_x = 0;
@@ -43,24 +58,30 @@ int pointer_y = 0;
 void setup() {
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     display.clearDisplay();
-    Serial.begin(9600);
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    Serial.begin(115200);
     pinMode(D3, INPUT_PULLUP);
     pinMode(D5, INPUT_PULLUP);
     pinMode(D7, INPUT_PULLUP);
-    ticker.attach(0.08, pointer);
+    connect();
+    udp.setTimeout(5);
+    ticker0.attach(0.08, pointer);
+    // ticker1.attach(5, parse);
 }
 
 void loop() {
     // compute fps
-    long dtime = micros() - time1;
-    time1 = micros();
-    double fps = 1000000.0/dtime;
-    Serial.println(fps);
-    //finish compute
+    // long dtime = micros() - time1;
+    // time1 = micros();
+    // double fps = 1000000.0 / dtime;
+    // Serial.println(fps);
+    // finish compute
     n++;
     drawPointer(pointer_x, pointer_y);
     swapchess();
     drawMap(n);
+    parse();
     display.display();
     display.clearDisplay();
 }
@@ -133,16 +154,23 @@ void drawPointer(int x0, int y0) {
     display.drawPixel(x + 1, y + 6, WHITE);
     display.drawPixel(x + 5, y + 6, WHITE);
     display.drawPixel(x + 6, y + 6, WHITE);
+    if (master == 1) {
+        display.setCursor(80, 0);
+        display.write("master");
+    } else {
+        display.setCursor(80, 0);
+        display.write("unmaster");
+    }
 }
 
-void swapchess(){
-    if(!digitalRead(D7) && ((millis() - time2) > 1000) && selected_lock == 0){
+void swapchess() {
+    if (!digitalRead(D7) && ((millis() - time2) > 1000) && selected_lock == 0) {
         time2 = millis();
         selected_lock = 1;
         selected_x = pointer_x;
         selected_y = pointer_y;
     }
-    if(!digitalRead(D7) && ((millis() - time2)>1000) && selected_lock == 1){
+    if (!digitalRead(D7) && ((millis() - time2) > 1000) && selected_lock == 1) {
         time2 = millis();
         selected_lock = 0;
         swap(&chessmap[selected_y][selected_x],
@@ -150,5 +178,62 @@ void swapchess(){
         if (chessmap[selected_y][selected_x] != 0) {
             chessmap[selected_y][selected_x] = 0;
         }
+        send();
+    }
+}
+
+void connect() {
+    const char *ssid = "ChineseChess";
+    const char *pawd = "18927858278";
+    WiFi.mode(WIFI_STA);
+    wifi.begin(ssid, pawd);
+    delay(15000);
+    if (wifi.status() != WL_CONNECTED) {
+        wifi.mode(WIFI_AP);
+        wifi.softAPConfig(mainIP, getawayIP, subnet);
+        wifi.softAP(ssid, pawd);
+        Serial.println("connect fail,start AP");
+        Serial.print("local IP: ");
+        Serial.println(wifi.softAPIP());
+        udp.begin(port);
+        master = 1;
+    } else {
+        Serial.println("connect success");
+        Serial.print("IP Adress: ");
+        Serial.println(WiFi.localIP());
+        udp.begin(port);
+        master = 0;
+        send();
+    }
+}
+
+void send() {
+    int *n = &chessmap[0][0];
+    if (master == 1) {
+        for (int i = 0; i < 90; i++) {
+            udp.beginPacket(udp.remoteIP(), port);
+            udp.print(*(n + i));
+            udp.endPacket();
+            delay(10);
+        }
+    } else {
+        for (int i = 0; i < 90; i++) {
+            udp.beginPacket(mainIP, port);
+            udp.print(*(n + i));
+            udp.endPacket();
+            delay(10);
+        }
+    }
+}
+
+void parse() {
+    if (udp.parsePacket()) {
+        int n = udp.parseInt();
+        Serial.println(n);
+        *(&chessmap[0][0] + i) = n;
+        i = i + 1;
+    }
+    if(i == 90){
+        i = 0;
     }
 }
---
+++ test/test.cpp
@@ -0,0 +1,60 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#ifndef STASSID
+#define STASSID "your-ssid"
+#define STAPSK "your-password"
+#endif
+
+unsigned int localPort = 8888; // local port to listen on
+
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1]; // buffer to hold incoming
+                                               // packet,
+char ReplyBuffer[] = "acknowledged\r\n"; // a string to send back
+
+WiFiUDP Udp;
+
+void setup() {
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(STASSID, STAPSK);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print('.');
+        delay(500);
+    }
+    Serial.print("Connected! IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.printf("UDP server on port %d\n", localPort);
+    Udp.begin(localPort);
+}
+
+void loop() {
+    // if there's data available, read a packet
+    int packetSize = Udp.parsePacket();
+    if (packetSize) {
+        Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d, "
+                      "free heap = %d B)\n",
+                      packetSize, Udp.remoteIP().toString().c_str(),
+                      Udp.remotePort(), Udp.destinationIP().toString().c_str(),
+                      Udp.localPort(), ESP.getFreeHeap());
+
+        // read the packet into packetBufffer
+        int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+        packetBuffer[n] = 0;
+        Serial.println("Contents:");
+        Serial.println(packetBuffer);
+
+        // send a reply, to the IP address and port that sent us the packet we
+        // received
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.write(ReplyBuffer);
+        Udp.endPacket();
+    }
+}
+
+/*
+  test (shell/netcat):
+  --------------------
+          nc -u 192.168.esp.address 8888
+*/
